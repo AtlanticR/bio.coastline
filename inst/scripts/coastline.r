dc125b714c095d7635c58c920b4be96baa81b013
Jae S Choi <choijaeseok@gmail.com>
2016-05-31 14:05:55 -0300

    name changes

--- inst/scripts/coastline.r
+++ inst/scripts/coastline.r
@@ -3,7 +3,7 @@
 
   p$project.root = project.datadirectory( p$project.name )
 
-  p$libs = ecomodLibrary( "bio.spacetime", "bio.utilities", "bio.bathymetry", "bio.coastline", "bio.polygons" )
+  p$libs = bioLibrary( "bio.spacetime", "bio.utilities", "bio.bathymetry", "bio.coastline", "bio.polygons" )
   p$libs = c(p$libs, RLibrary( "rgdal", "maps", "mapdata", "maptools", "geosphere", "sp", "raster", "rgeos" ) )
 
   # default (= only supported resolution of 0.5 km discretization)  .. do NOT change
